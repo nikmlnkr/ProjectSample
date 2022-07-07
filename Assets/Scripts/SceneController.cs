6755b48e500877c2afa7eac51c4ebcf9713eebe4
nikmlnkr <nikmlnkr@gmail.com>
2022-07-07 19:54:55 +0530

    Base commit

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    Scene currentScene;
+
+    void Start()
+    {
+        currentScene = SceneManager.GetActiveScene();
+        Debug.Log("Our scene name is " + currentScene.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.V))
+        {
+            if(currentScene.name == "PoleScene")
+            {
+                SceneManager.LoadScene("FirstLevel");
+            }
+            else
+            {
+                SceneManager.LoadScene("PoleScene");
+            }
+        }
+    }
+}
