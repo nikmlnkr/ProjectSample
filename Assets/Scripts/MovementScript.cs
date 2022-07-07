66a29c213d649117c86c21704fe8d45835f309b0
nikmlnkr <nikmlnkr@gmail.com>
2022-07-07 20:56:57 +0530

    Added new variables and changed MovementScript

--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -5,6 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class MovementScript : MonoBehaviour
 {
+    public float speed = 0.03f;
+    public float rotateSpeed = 0.4f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,14 +16,24 @@ public class MovementScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(speed, 0, 0);
+        }
+
+        if(Input.GetKey(KeyCode.S))
+        {
+            transform.position -= new Vector3(speed, 0, 0);
+        }
+
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(0, 1, 0);
+            transform.Rotate(0, rotateSpeed, 0);
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(0, -1, 0);
+            transform.Rotate(0, -rotateSpeed, 0);
         }
 
         //M de-activates and activates visibility of the object
