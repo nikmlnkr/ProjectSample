fa96fc72cbc285e4aa3ae9e11757d78e6324a706
nikmlnkr <nikmlnkr@gmail.com>
2022-07-18 21:00:50 +0530

    Added camera follow

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset;
+
+    public float followSpeed = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, player.position - offset, Time.deltaTime * followSpeed);
+    }
+}
